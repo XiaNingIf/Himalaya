622088ab74252b676e3eb918f2787cf70b96ee09
XiaNingIf <1304701184@qq.com>
2020-07-05 12:56:33 +0800

    Initial commit

---
+++ app/src/main/java/com/jit/himalaya/MainActivity.java
@@ -0,0 +1,48 @@
+package com.jit.himalaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<String, String>();
+        IDataCallBack<CategoryList> newIDataCallBack;
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories!=null){
+                    int size = categories.size();
+                    Log.e(TAG,"categories.size ---> " + size);
+                    for (Category category:categories){
+                        Log.e(TAG,"category ---> " + category.getCategoryName());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                    Log.e(TAG,"error code --- >" + i +"error message "+s);
+            }
+        });
+    }
+
+}
